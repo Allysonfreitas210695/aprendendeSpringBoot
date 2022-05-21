cb0451b66af792a9512b860be663aee6629eb58b
Allysonfreitas210695 <alisonfr83@gmail.com>
2022-05-21 02:33:56 -0300

    Requisição GET com parâmentros e Response Entity

--- src/main/java/com/gerenciamento/tarefa/TarefaApplication.java
+++ src/main/java/com/gerenciamento/tarefa/TarefaApplication.java
@@ -1,12 +1,24 @@
 package com.gerenciamento.tarefa;
 
+import com.gerenciamento.tarefa.domain.entity.Cliente;
+import com.gerenciamento.tarefa.repository.Clientes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TarefaApplication {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+		return args -> {
+			clientes.save(new Cliente("allyson"));
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TarefaApplication.class, args);
